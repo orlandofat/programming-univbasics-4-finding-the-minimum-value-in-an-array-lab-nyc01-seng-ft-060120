1973440e0143533ab5b8d20ac65fcb06cc02355c
Orlando Challenger <orlandofat@gmail.com>
2020-05-13 17:02:29 +0000

    Automatically backed up by Learn

--- lib/find_the_minimum_value.rb
+++ lib/find_the_minimum_value.rb
@@ -1,3 +1,13 @@
 def find_min_value(array)
   # Add your solution here
+  count = 0
+  min_value = 1+
+  while count < array.length do
+    if min_value == array[count]
+      min_value = array[count]
+    end
+    count 
 end
+min_value
+end
+
